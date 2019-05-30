2aaf446d167f34fbd55666f4ee0879d0f935e502
malu2001 <marialuisamognon7@gmail.com>
2019-05-30 15:55:26 -0300

    atualizando

--- classe_flask.py
+++
@@ -1,5 +0,0 @@
-class Pessoa():
-    def __init__(self, nome, endereco, telefone):
-        self.nome = nome
-        self.endereco = endereco
-        self.telefone = telefone
--- form_atualiza_pessoa.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask , render_template
-app = Flask (name)
-@app.route(”/form_atualizar_pessoa”)
-def form_atualizar_pessoa():
-return render_template(’hello.html’)
-app.run()
