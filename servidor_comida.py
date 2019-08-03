ab94d2212df7475d3a4f27549fe96a1b45501b19
malu2001 <marialuisamognon7@gmail.com>
2019-08-03 15:00:54 -0300

    servidor comidas

--- servidor_comida.py
+++ servidor_comida.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, redirect
 from peewee import *
 import os
 
@@ -14,7 +14,7 @@ class Comida(BaseModelo):
     categoria = CharField()
     vencimento = CharField()
 
-lista_global = Comida.select()
+lista_global = []
 app = Flask(__name__)
 
 @app.route("/")
@@ -28,18 +28,28 @@ def listar_comidas():
 
 @app.route("/inserir_comida", methods = ['POST'])
 def inserir_comida():
-
     Comida.create (nome = request.form ["nome"], categoria = request.form ["categoria"], vencimento = request.form["vencimento"])
     return render_template("exibir_comidas.html", lista_comidas = Comida.select())
 
-@app.route("/excluir_comida", methods = ['POST'])
+@app.route("/excluir_comida")
 def excluir_comida():
-    Comida(nome = request.form["nome"])
-    for i in Comida.select():
-        if i.nome == Comida.nome:
-            Comida.delete_instance(i)
-    return render_template("exibir_comidas.html", lista_comidas = Comida.select)
+    Comida.delete_by_id(request.args.get("id"))
+    return redirect("/")
 
+@app.route("/form_alterar_comida")
+def form_alterar_comida():
+    pegar_comida = Comida.get_by_id(request.args.get("id"))
+    return render_template("alterar_comida.html", comida = pegar_comida)
+
+@app.route("/alterar_comida", methods = ["POST"])
+def alterar_comida():
+    comida = Comida.get_by_id(request.form["id"])
+    comida.nome = request.form["nome"]
+    comida.categoria = request.form["categoria"]
+    comida.vencimento = request.form["vencimento"]
+    comida.save()
+    return render_template("listar_comida.html")
+   
 db.connect()
 db.create_tables([Comida])
 
