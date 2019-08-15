436f21186aa84aff0fe818f198a7cd55d32141fe
malu2001 <marialuisamognon7@gmail.com>
2019-08-15 17:25:51 -0300

    Atualizando

--- ex48_aprimoramento_planta.py
+++ ex48_aprimoramento_planta.py
@@ -8,27 +8,31 @@ class BaseModelo(Model):
     class Meta:
         database = db
 
-class Especie(BaseModelo):
-    periodo_poda = CharField()
-    data_plantacao = CharField()
-
 class Planta(BaseModelo):
     nome_planta = CharField()
     nome_cientifico = CharField()
     tam_folha = CharField()
-    especie = ForeignKeyField(Especie)
+    periodo_poda = CharField()
 
 class Jardim(BaseModelo):
     nome = CharField()
-    plantas = ManyToManyField(Planta)
+
+class PlantaDoJardim(BaseModelo):
+    data_plantacao = CharField()
+    planta = ForeignKeyField(Planta)
+    jardim = ForeignKeyField(Jardim)
 
 if os.path.exists(arq):
     os.remove(arq)
 
 db.connect()
-db.create_tables([Especie, Planta, Jardim, Jardim.plantas.get_through_model()])
+db.create_tables([Planta, Jardim, PlantaDoJardim])
 
-espe = Especie.create(periodo_poda = "3 dias", data_plantacao = "12/08/2017")
-rosa = Planta.create(nome_planta = "Rosa", nome_cientifico = "Plantae rosium", tam_folha = "medio", especie = espe)
+rosa = Planta.create(nome_planta = "Rosa", nome_cientifico = "Plantae rosium", tam_folha = "medio", periodo_poda = "120 dias")
 jar = Jardim.create(nome = "Jardim la de casa")
+plant_jar = PlantaDoJardim.create(data_plantacao = "12/02/2019", planta = rosa, jardim = jar)
 
+for i in PlantaDoJardim.select():
+    print("Data plantacao: " +str(i.data_plantacao))
+    print("Dados da planta: " +str(i.planta.nome_planta) + " | " +str(i.planta.nome_cientifico ) + " | " + str(i.planta.tam_folha ) + " | " +(i.planta.periodo_poda))
+    print("Nome do Jardim: " + str(i.jardim.nome))
